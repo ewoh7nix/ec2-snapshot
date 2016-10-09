67acfd48f19c8f69b8e62f2485a9a7c16eb8d4cd
nasrullah <nasrullah@hippy.local>
2016-10-10 00:03:22 +0700

    initial code

---
+++ ec2-snapshot.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# @author: Nasrullah <ewoh7nix@gmail.com>
+# @verion: 0.1
+
+import boto3
+import re
+
+ec2 = boto3.resource('ec2')
+
+def create_snapshot():
+
+def delete_snapshot():
+
+
+def main():
+
+if '__name__' == '__main__':
+    main()
